f35ab91f8f163583b2997f4281e224ec2d425264
LiviuCauia <li.viu@mail.com>
2023-01-30 10:19:31 +0000

    Microsoft MVC tutorial

---
+++ MvcMovie.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Web.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie.Web/Data/MvcMovieWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Web.Models;
+
+namespace MvcMovie.Web.Data
+{
+    public class MvcMovieWebContext : DbContext
+    {
+        public MvcMovieWebContext (DbContextOptions<MvcMovieWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Web.Models.Movie> Movie { get; set; } = default!;
+    }
+}
